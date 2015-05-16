678d6861658c5901e2fcf1464e9cc81e5e12c4f5
Larry Kluger <github@kluger.com>
2015-05-16 22:54:29 +0300

    modal working

--- src/js/register-main.js
+++ src/js/register-main.js
@@ -266,7 +266,7 @@ $(document).ready(function () {
 	}
 	function working(show) {
 		if (show) {
-			$('#working').modal();
+			$('#working').modal({keyboard: false});
 		} else {
 			$('#working').modal('hide');
 		}
