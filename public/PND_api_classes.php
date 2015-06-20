fe7c5b95f5532adbdb2cb536ce104530753f1344
Larry Kluger <github@kluger.com>
2015-06-20 22:31:42 +0300

    Add api classes

---
+++ public/PND_api_classes.php
@@ -0,0 +1,57 @@
+<?php
+
+#	api classes for the Push Notifications DocuSign app
+# The following chain-of-command pattern is from 
+# http://www.ibm.com/developerworks/library/os-php-designptrns/
+
+interface PND_Request
+{
+  function request( $op );
+}
+
+class PND_HandlerChain {
+  private $_handlers = array();
+
+  public function addHandler( $handler ) {
+    $this->_handlers []= $handler;
+  }
+
+  public function handle( $op ) {
+    global $pnd_utils;
+	
+	foreach( $this->_handlers as $handler ) {
+      if ( $handler->request( $op ) )
+        return;
+    }
+	
+	# bad op
+	$pnd_utils->return_data( 
+		array( 'bad_data' => array(), 'msg' => 'Bad op' ), 501); # 501 - Not implemented
+  }
+}
+
+class PND_API {
+	# API Support
+	private $_email = null;
+	private $_pw = null;
+	
+	public function email() {return $this->_email;}
+	public function pw() {return $this->_pw;}
+  
+	public function check_email_pw() {
+		global $pnd_utils;
+		if (!isset($_POST['email']) || strlen($_POST['email']) < 1) {
+			$pnd_utils->return_data( 
+				array( 'api' => true, 'bad_data' =>array('email'), 'msg' => 'Please enter your email address' ), 400);
+			return false;
+		}
+		if (!isset($_POST['pw']) || strlen($_POST['pw']) < 1) {
+			$pnd_utils->return_data( 
+				array( 'api' => true, 'bad_data' => array('pw'), 'msg' => 'Please enter your password' ), 400);
+			return false;
+		}
+		$this->_email = $_POST['email'];
+		$this->_pw = $_POST['pw'];
+		return true;
+	}
+}
--- public/bootstrap.php
+++ public/bootstrap.php
@@ -23,6 +23,7 @@ require 'vendor_lsk/DocuSign-PHP-Client/src/service/DocuSign_LoginService.php';
 require 'vendor_lsk/DocuSign-PHP-Client/src/service/DocuSign_UserService.php';
 
 require (LIB_PATH . "PND_utils.php");
+require (LIB_PATH . "PND_api_classes.php");
 require (LIB_PATH . "PND_op_authenticate.php");
 require (LIB_PATH . "PND_op_subscribe.php");
 
