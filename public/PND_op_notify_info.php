749b324cffd861ae94c1df5788c56d74cdab866a
Larry Kluger <github@kluger.com>
2015-09-08 20:53:02 +0300

    Matching the api with the service worker's expectations

--- public/PND_op_notify_info.php
+++ public/PND_op_notify_info.php
@@ -39,11 +39,12 @@ class PND_op_notify_info implements PND_Request
 	$pnd_utils->return_data([
 		'cookie_id' => $cookie_id, 
 		'error' => false,
-		'notification' => true,
-		'title' => 'XYZ document signed!', 
-		'message' => 'This is the message. Cookie: ' . $cookie_id . ' This is the message. This is the message. This is the message.' , 
-		'icon' => true,
-		'notificationTag' => 123
+		'notification' => [
+			'title' => 'XYZ document signed!', 
+			'message' => 'This is the message. Cookie: ' . $cookie_id . ' This is the message. This is the message. This is the message.' , 
+			'icon' => true,
+			'notificationTag' => 123
+			]
 		], 200);
 	return true;
 
